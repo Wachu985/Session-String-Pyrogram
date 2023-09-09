b35356cba1a91eaf7f0c37f54df4b35745dd63ba
Wachu985 <pedrobonilla985@gmail.com>
2023-09-08 22:38:43 -0400

    fix: Fixed error for windows and 2fa

--- session_string_pyrogram.py
+++ session_string_pyrogram.py
@@ -4,7 +4,7 @@ import asyncio
 async def stringGenerate():
     api_id = input("Api ID: \n")
     api_hash = input("Api Hash: \n")
-    client = Client(":memory:", api_id, api_hash)
+    client = Client("memory", api_id, api_hash)
     await client.connect()
     
     phone_number = input("Insert your phone number: \n")
@@ -13,16 +13,13 @@ async def stringGenerate():
     
     phone_code_msg = input("Insert registration code: \n")
     
-    await client.sign_in(phone_number, code.phone_code_hash, phone_code_msg)
-    
-    bolean = input("You have double factor? y/n \n")
-    
-    if bolean == 'y':
+    try:
+        print('Login in Telegram...')
+        await client.sign_in(phone_number, code.phone_code_hash, phone_code_msg)
+    except:
         password = input('Insert your double factor:\n')
         await client.check_password(password=password)
-        
-    
-    
+    print("Session String: ")
     string_session = await client.export_session_string()
     
     
